f2de8d7ffa1f1047d80f2e2f8ec68c1639433ba0
Sean Goedecke <sgoedecke@zendesk.com>
2018-06-03 13:32:21 +1000

    initial commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+  "fmt"
+  "github.com/PuerkitoBio/goquery"
+  "io/ioutil"
+  "math/rand"
+  "net/http"
+  "strings"
+  "time"
+  "flag"
+  "strconv"
+  "github.com/sgoedecke/mashup/markov"
+)
+
+func main() {
+  numSentencesPtr := flag.String("n", "3", "number of sentences to generate")
+  flag.Parse()
+  rand.Seed(time.Now().Unix())
+
+  dict := NewMarkov()
+  fmt.Println(flag.Args())
+  for _, url := range(flag.Args()) {
+    resp, err := http.Get(url)
+    if err != nil {
+      panic(err)
+    }
+    text := ExtractText(resp)
+    dict.Add(text)
+  }
+
+  numSentences, _ := strconv.Atoi(*numSentencesPtr)
+  fmt.Println(dict.GenerateParagraph(numSentences))
+}
+// extract only the text from a http response
+func ExtractText(resp *http.Response) string {
+  text, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    panic(err)
+  }
+  resp.Body.Close()
+  s := string(text)
+
+  p := strings.NewReader(s)
+  doc, _ := goquery.NewDocumentFromReader(p)
+
+  doc.Find("script").Each(func(i int, el *goquery.Selection) {
+    el.Remove()
+  })
+
+  doc.Find("styles").Each(func(i int, el *goquery.Selection) {
+    el.Remove()
+  })
+  doc.Find("head").Each(func(i int, el *goquery.Selection) {
+    el.Remove()
+  })
+
+  return doc.Text()
+}
