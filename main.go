0f3dbaf7bb6cd744c659eb80f3dc68e039094080
Sean Goedecke <sgoedecke@zendesk.com>
2018-06-03 13:35:50 +1000

    go fmt

--- main.go
+++ main.go
@@ -1,59 +1,60 @@
 package main
 
 import (
-  "fmt"
-  "github.com/PuerkitoBio/goquery"
-  "io/ioutil"
-  "math/rand"
-  "net/http"
-  "strings"
-  "time"
-  "flag"
-  "strconv"
-  "github.com/sgoedecke/mashup/markov"
+	"flag"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/sgoedecke/mashup/markov"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-  numSentencesPtr := flag.String("n", "3", "number of sentences to generate")
-  flag.Parse()
-  rand.Seed(time.Now().Unix())
+	numSentencesPtr := flag.String("n", "3", "number of sentences to generate")
+	flag.Parse()
+	rand.Seed(time.Now().Unix())
 
-  dict := NewMarkov()
-  fmt.Println(flag.Args())
-  for _, url := range(flag.Args()) {
-    resp, err := http.Get(url)
-    if err != nil {
-      panic(err)
-    }
-    text := ExtractText(resp)
-    dict.Add(text)
-  }
+	dict := NewMarkov()
+	fmt.Println(flag.Args())
+	for _, url := range flag.Args() {
+		resp, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		text := ExtractText(resp)
+		dict.Add(text)
+	}
 
-  numSentences, _ := strconv.Atoi(*numSentencesPtr)
-  fmt.Println(dict.GenerateParagraph(numSentences))
+	numSentences, _ := strconv.Atoi(*numSentencesPtr)
+	fmt.Println(dict.GenerateParagraph(numSentences))
 }
+
 // extract only the text from a http response
 func ExtractText(resp *http.Response) string {
-  text, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    panic(err)
-  }
-  resp.Body.Close()
-  s := string(text)
+	text, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	s := string(text)
 
-  p := strings.NewReader(s)
-  doc, _ := goquery.NewDocumentFromReader(p)
+	p := strings.NewReader(s)
+	doc, _ := goquery.NewDocumentFromReader(p)
 
-  doc.Find("script").Each(func(i int, el *goquery.Selection) {
-    el.Remove()
-  })
+	doc.Find("script").Each(func(i int, el *goquery.Selection) {
+		el.Remove()
+	})
 
-  doc.Find("styles").Each(func(i int, el *goquery.Selection) {
-    el.Remove()
-  })
-  doc.Find("head").Each(func(i int, el *goquery.Selection) {
-    el.Remove()
-  })
+	doc.Find("styles").Each(func(i int, el *goquery.Selection) {
+		el.Remove()
+	})
+	doc.Find("head").Each(func(i int, el *goquery.Selection) {
+		el.Remove()
+	})
 
-  return doc.Text()
+	return doc.Text()
 }
